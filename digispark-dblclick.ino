5d7bf25245709b8134973c1ca82195daa4e2aec4
Gregory <me@gpaul.me>
2022-06-15 18:25:02 +0200

    Make it click !

--- digispark-dblclick.ino
+++ digispark-dblclick.ino
@@ -1,32 +1,43 @@
 #include "DigiMouse.h"
 
-#define DELAY 250
+#define LED 1
+#define BUTTON 2
 
 // https://github.com/digistump/DigisparkArduinoIntegration/blob/master/libraries/DigisparkMouse/examples/Mouse/Mouse.ino
+// https://www.logiqbit.com/perfect-arduino-button-presses
 
+bool button_debounce = false; 
 bool active = false;
+int counter = 0;
 
 void blink() { 
-  pinMode(1, OUTPUT); // LED
-  pinMode(2, INPUT); // Button
-  digitalWrite(1, HIGH);
-  DigiMouse.delay(500);
-  digitalWrite(1, LOW); 
+  digitalWrite(LED, HIGH);
+  DigiMouse.delay(100);
+  digitalWrite(LED, LOW); 
 }
 
 void setup() {
-  pinMode(1, OUTPUT); // LED
   DigiMouse.begin();
+  pinMode(LED, OUTPUT); // LED
+  pinMode(BUTTON, INPUT); // Button
   blink();
 }
 
 void loop() {
-  if (digitalRead(2) == LOW) {
-    blink();
+  if (digitalRead(BUTTON) == LOW && button_debounce == false) {
+    button_debounce = true;
     active = !active;
+    counter = 0;
+    DigiMouse.setButtons(0);
+    DigiMouse.delay(100);
   }
+  if (digitalRead(BUTTON) == HIGH && button_debounce == true) {
+    button_debounce = false;  // this will prevent subsequent presses
+  }
+  digitalWrite(LED, active ? HIGH : LOW);
   if (active) {
-    DigiMouse.leftClick();
+    counter = counter < 10 ? counter + 1 : 0;
+    DigiMouse.setButtons(counter < 5 ? 1<<0 : 0);
   }
-  DigiMouse.delay(DELAY);
+  DigiMouse.delay(10);
 }
